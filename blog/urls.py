1c50e920839298e7693fc5358a9525e6aa62e2a7
Watanabe Hidetoshi <hide.watanabe@socan.co.jp>
2023-05-08 11:30:52 +0900

    パスワード変更部分実装

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 #cording:utf-8
-from django.urls import path
+from django.urls import path,re_path,include
 from . import views
 urlpatterns=[
     path('',views.post_list,name='post_list'),
+    path('accounts/',include('django.contrib.auth.urls')),
 ]
