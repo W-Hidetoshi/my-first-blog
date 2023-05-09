9e844e6f4a059b6c35ac348296d94e38efe69ac6
Watanabe Hidetoshi <hide.watanabe@socan.co.jp>
2023-05-09 18:00:25 +0900

    Added views to create/edit blog inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ urlpatterns=[
     path('',views.post_list,name='post_list'),
     path('accounts/',include('django.contrib.auth.urls')),
     path('post/<int:pk>',views.post_detail,name='post_detail'),
-    path('post/new', views.post_new, name = 'post_new'),
+    path('post/new/', views.post_new, name = 'post_new'),
     path('post/<int:pk>/edit',views.post_edit,name = 'post_edit'),
 ]
 
